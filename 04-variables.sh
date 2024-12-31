17793e27cc6602903220835f873ae44e47d7cde2
subbareddy <subbareddych494@gmail.com>
2024-12-31 16:25:28 +0530

    conversation added

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1 # no space before and after equal
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2 :: Hi $PERSON1"
+echo "$PERSON1:: how are you doing?"
+echo "$PERSON2:: I am good.how are you?"
