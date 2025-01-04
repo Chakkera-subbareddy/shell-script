2f52bdc8bf98ca684ccd66e8ddd93598de14a546
subbareddy <subbareddych494@gmail.com>
2025-01-04 20:47:34 +0530

    Functions

--- 12-colors.sh
+++ 12-colors.sh
@@ -8,10 +8,10 @@ Y="\e[33m"
 Validate(){
      if [ $1 -ne 0 ]
         then 
-            echo "$2...$R FAILURE"
+            echo -e "$2...$R FAILURE"
             exit 1
         else 
-            echo "$2...$G SUCCESS"
+            echo -e "$2...$G SUCCESS"
         fi
 }
  
@@ -38,7 +38,7 @@ dnf list installed Git
 else 
      echo -e "Git is already ...$Y INSTALLED"
  fi 
- 
+
  
 
 
