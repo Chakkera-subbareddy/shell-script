55067210652d96a7b9e38655462738e0d2972f14
subbareddy <subbareddych494@gmail.com>
2025-01-04 20:47:04 +0530

    Functions

---
+++ 12-colors.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+Validate(){
+     if [ $1 -ne 0 ]
+        then 
+            echo "$2...$R FAILURE"
+            exit 1
+        else 
+            echo "$2...$G SUCCESS"
+        fi
+}
+ 
+ if [ $USERID -ne 0 ]
+  then 
+     echo "ERROR:: you must have sudo access to execute this script"
+    exit 1 #other than o
+fi 
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then  # not installed 
+        dnf install mysql -y
+        Validate $? "Installing MySQL"    
+ else 
+            echo -e "MYSQL is already ...$Y INSTALLED"
+ fi 
+
+dnf list installed Git
+ if [ $? -ne 0 ] 
+ then # not installed 
+        dnf install Git -y
+        Validate $? "Installing Git"      
+else 
+     echo -e "Git is already ...$Y INSTALLED"
+ fi 
+ 
+ 
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
