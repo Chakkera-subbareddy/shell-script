348a21bddf8e92a294bd6e76125eefc9636e4a36
subbareddy <subbareddych494@gmail.com>
2025-01-04 22:47:23 +0530

    Functions

--- 13-logs.sh
+++ 13-logs.sh
@@ -4,6 +4,9 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+
+N="\e[0m"
+
 LOGS_FOLDER="/var/log/shell-script-logs"
 LOGS_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
@@ -12,10 +15,10 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP.log"
 Validate(){
      if [ $1 -ne 0 ]
         then 
-            echo -e "$2...$R FAILURE"
+            echo -e "$2...$R FAILURE $N"
             exit 1
         else 
-            echo -e "$2...$G SUCCESS"
+            echo -e "$2...$G SUCCESS $N"
         fi
 }
 
@@ -33,7 +36,7 @@ then  # not installed
         dnf install mysql -y &>>$LOG_FILE_NAME
         Validate $? "Installing MySQL"    
  else 
-            echo -e "MYSQL is already ...$Y INSTALLED"
+            echo -e "MYSQL is already ...$Y INSTALLED $N"
  fi 
 
 dnf list installed Git &>>$LOG_FILE_NAME
@@ -42,26 +45,7 @@ dnf list installed Git &>>$LOG_FILE_NAME
         dnf install Git -y &>>$LOG_FILE_NAME
         Validate $? "Installing Git"      
 else 
-     echo -e "Git is already ...$Y INSTALLED"
+     echo -e "Git is already ...$Y INSTALLED $N"
  fi 
-
-
-
-
-
-
  
-
-
- 
-
-
-
-
-
-
-
-
-
-
 
