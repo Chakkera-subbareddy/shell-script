36533693e54e0c5c4f7c6eb87b8371f603595593
subbareddy <subbareddych494@gmail.com>
2025-01-07 13:30:21 +0530

    ERROR-HANDLING

--- 18-backup.sh
+++ 18-backup.sh
@@ -28,5 +28,12 @@ Validate(){
 
 USAGE(){
     echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
-    exit 1
 }
+
+mkdir -p /home/ec2-user/shellscript-logs/
+if [ $# -lt 2]
+then 
+    USAGE
+fi 
+
+echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
