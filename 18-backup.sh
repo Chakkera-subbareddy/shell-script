3f9285598225228693e84df61c8d7d4f6d9fa5f3
subbareddy <subbareddych494@gmail.com>
2025-01-07 13:22:15 +0530

    ERROR-HANDLING

--- 18-backup.sh
+++ 18-backup.sh
@@ -11,7 +11,7 @@ SOURCE_DIR=$1
 DEST_DTR=$2
 DAYS=${3:-14} # if user is not providing no of days, we are taking default 14 days
 
-LOGS_FOLDER="/var/log/shellscript-logs"
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
 LOGS_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP.log"
@@ -28,10 +28,5 @@ Validate(){
 
 USAGE(){
     echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    exit 1
 }
-if [ $# -lt 2]
-then 
-    USAGE
-fi 
-
-echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
