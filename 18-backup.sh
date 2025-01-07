265ede828f4d543d15832485c7a0bec597fc6615
subbareddy <subbareddych494@gmail.com>
2025-01-07 14:04:17 +0530

    ERROR-HANDLING

--- 18-backup.sh
+++ 18-backup.sh
@@ -51,5 +51,5 @@ fi
 
 echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-FILES=$(find $SOURCE_DIR -name "*.log" +mtime $DAYS)
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 echo "Files are: $FILES"
