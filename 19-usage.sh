409a1a6a5c62b89407eb5e1c5016ecf6b8318e12
subbareddy <subbareddych494@gmail.com>
2025-01-08 22:40:42 +0530

    ERROR-HANDLING

--- 19-usage.sh
+++ 19-usage.sh
@@ -5,5 +5,7 @@ DISk_THRESHOLD=s #real projects will monitor for 70
 
 while read -r line
 do 
-  USAGE=$(echo $line | awk -f " " '{print $6F}')
+  USAGE=$(echo $line | awk -f " " '{print $6F}' | cut -d "%" -f1)
+  PARTITION=$(echo $line | awk -f " " '{print $6F}')
+  echo "Partition: $PARTITION , Usage: $USAGE"
 done <<< $DISK_USAGE
